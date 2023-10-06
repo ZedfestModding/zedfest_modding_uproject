a8f40dc2a3dcf1b36406b704be66f145dacb8e3f
Mythical-Github <MythicalData@gmail.com>
2023-10-06 10:08:01 -0400

    Cleanup

--- KevinSpel/Plugins/VictoryPlugin422/Intermediate/Build/Win64/KevinSpel/Development/VictoryBPLibrary/PCH.VictoryBPLibrary.cpp
+++
@@ -1,1 +0,0 @@
-#include "D:/Unreal Engine Installs/UnrealEngine-4.22/Engine/Plugins/MythicalModdingFramework/VictoryPlugin422/Intermediate/Build/Win64/KevinSpel/Development/VictoryBPLibrary/PCH.VictoryBPLibrary.h"
--- KevinSpel/Plugins/VictoryPlugin422/Intermediate/Build/Win64/UE4Editor/Development/VictoryBPLibrary/PCH.VictoryBPLibrary.cpp
+++
@@ -1,1 +0,0 @@
-#include "D:/Unreal Engine Installs/UnrealEngine-4.22/Engine/Plugins/MythicalModdingFramework/VictoryPlugin422/Intermediate/Build/Win64/UE4Editor/Development/VictoryBPLibrary/PCH.VictoryBPLibrary.h"
